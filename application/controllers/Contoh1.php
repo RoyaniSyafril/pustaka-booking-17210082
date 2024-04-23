e4527d2df212737121b945e3cb13bbeeab4767da
royanisyafril <royani.syafril@gmail.com>
2024-04-23 13:14:29 +0700

    first commit

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,26 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Contoh1 extends CI_Controller {
+
+	
+	public function index()
+	{
+		echo"<h1>PERKENALAN</h1>";
+        echo"Nama Saya : Royani Syafril <br>
+        Kelas : 17.6A.27 <br>
+        Program Studi : Teknologi informasi <br>
+        Semester : 6 <br>
+        Alamat : Jl. Bhayangkara E 27/7 Benda baru, Pamulang - Tangsel <br>";
+	}
+
+    public function latihan()
+	{
+		echo"<h1>PERKENALAN</h1>";
+        echo"Nama Saya : Royani Syafril <br>
+        Kelas : 17.6A.27 <br>
+        Program Studi : Teknologi informasi <br>
+        Semester : 6 <br>
+        Alamat : Jl. Bhayangkara E 27/7 Benda baru, Pamulang - Tangsel <br>";
+	}
+}
---
+++ application/views/v_hubungi_kami.php
@@ -0,0 +1,14 @@
+<section>
+ <h1><?php echo $judul ?></h1>
+ <h1>Sosial Media</h1>
+ <ul type="disc">
+ <li>Facebook : Royani Syafril</li>
+ <li>Intagram : Royani.syafril</li>
+ </ul>
+ 
+ <h1>Call Center</h1>
+ <ul type="disc">
+ <li>Phone 0821 - 1209 - 9856</li>
+ <li>WhatsApp 0821 - 1209 - 9856</li>
+</ul>
+</section>
